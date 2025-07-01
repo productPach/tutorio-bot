262632cc8e8d02ec3f8745b9fcff67df1205e36a
fedotovps <fedotovps@bk.ru>
2025-07-02 00:44:49 +0300

    abrc 11

--- commands/text.js
+++ commands/text.js
@@ -93,7 +93,7 @@ module.exports = async function text(ctx) {
 
   try {
     // Получаем ответ OpenAI
-    const reply = await getChatCompletion(
+    let reply = await getChatCompletion(
       optimizedHistory,
       user.selectedSubject,
       user.selectedMode
